f9398fb852f6c3425ddad7841f8744f4298edf39
anongxa <61239936+anongxa@users.noreply.github.com>
2021-05-16 21:42:33 +0200

    new project

---
+++ src/main/java/az/ac/cput/Stationary.java
@@ -0,0 +1,25 @@
+package az.ac.cput;
+
+
+/**
+ * @author anongxa
+ * Student No:204513723
+ */
+
+public class Stationary {
+    private int id;
+    private String stationaryName;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getStationaryName() {
+        return stationaryName;
+    }
+    public void setStationaryName(String stationaryName) {
+        this.stationaryName = stationaryName;
+    }
+
+}
